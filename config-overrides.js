e19bc3fb7e9959cb0cd976dc3e52655dbe087037
Svilen Hadzhiev <Svilen_Hadzhiev@epam.com>
2018-08-23 12:45:08 +0300

    configured app to do not open browser on start added check for development and production mode and a way to override webpack configuration

--- config-overrides.js
+++ config-overrides.js
@@ -2,6 +2,13 @@ const rewireReactHotLoader = require('react-app-rewire-hot-loader');
 
 /* config-overrides.js */
 module.exports = function override (config, env) {
-  config = rewireReactHotLoader(config, env)
-  return config
+  if(env === "development") {
+    env.BROWSER = 'none';
+    console.log('development');
+  }
+  if(env === "production") {
+    console.log('production');
+  }
+  config = rewireReactHotLoader(config, env);
+  return config;
 }
